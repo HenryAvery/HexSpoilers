17e331ce578eacab6facdd77a36b63354fcb2995
Henry Avery <henryaverydev@gmail.com>
2017-12-09 05:52:16 +0000

    add edit and update route

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express    = require("express"),
+      methodOverride = require("method-override"),
       bodyParser = require("body-parser"),
       mongoose   = require("mongoose"),
       app        = express(),
@@ -8,12 +9,14 @@ mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/hextcg_spoilers");
 app.use(bodyParser.urlencoded({extended: true}));   
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs");      
 
 //Landing Page
 app.get("/", (req, res) => {
     res.render("landing");
 });
+
 
 //Index
 app.get("/cards", (req, res) => {
@@ -54,7 +57,27 @@ app.get("/cards/:id", (req, res) => {
     });
 });
      
-     
+//Edit
+app.get("/cards/:id/edit", (req, res) => {
+    Card.findById(req.params.id, (err, foundCard) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.render("cards/edit", {card: foundCard});
+        }
+    });
+});
+
+//Update
+app.put("/cards/:id", (req, res) => {
+    Card.findByIdAndUpdate(req.params.id, req.body.card, (err, foundCard) => {
+        if(err) {
+            console.log(err);
+        }else{
+            res.redirect("/cards/" + req.params.id);
+        }     
+    });
+});
      
       
 app.listen(process.env.PORT, process.env.IP, function(){
