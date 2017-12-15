a051d202c39e98e8ed600a4ea1c02a574f96b20a
Henry Avery <henryaverydev@gmail.com>
2017-12-15 09:14:17 +0000

    add edit update and destroy routes and buttons

--- routes/comments.js
+++ routes/comments.js
@@ -4,7 +4,7 @@ const express = require("express"),
        router = express.Router({mergeParams: true});
        
        
-//comments new
+//New
 router.get("/new", (req, res) => {
     Card.findById(req.params.id, (err, card) => {
         if(err){
@@ -15,7 +15,7 @@ router.get("/new", (req, res) => {
     });
 });
 
-//comments create
+//Create
 router.post("/", (req, res) => {
    Card.findById(req.params.id, (err, card) => {
        if(err){
@@ -24,7 +24,7 @@ router.post("/", (req, res) => {
            Comment.create(req.body.comment, (err, comment) => {
               if(err){
                   console.log(err);
-              } else {
+              }else{
                   card.comments.push(comment);
                   card.save();
                   res.redirect("/cards/" + card._id);
@@ -32,6 +32,45 @@ router.post("/", (req, res) => {
            });
        }
    }); 
+});
+
+//Edit
+router.get("/:comment_id/edit", (req, res) => {
+    Card.findById(req.params.id, (err, foundCard) => {
+        if(err){
+            console.log(err);
+        }else{
+            Comment.findById(req.params.comment_id, (err, foundComment) => {
+                if(err){
+                    console.log(err);
+                }else{
+                    res.render("comments/edit", {card_id: req.params.id, comment: foundComment});
+                }
+            });
+        }
+    });
+});
+
+//Update
+router.put("/:comment_id", (req, res) => {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment ) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/cards/" + req.params.id);
+        }
+    });
+});
+
+//Destroy
+router.delete("/:comment_id", (req, res) => {
+    Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/cards/" + req.params.id);
+        }
+    });
 });
 
 
