edf51790efdb38a3d5824d8e116f0c0abe6ef757
Henry Avery <henryaverydev@gmail.com>
2017-12-16 08:33:50 +0000

    add passport and register functionality

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const express    = require("express"),
-      methodOverride = require("method-override"),
-      bodyParser = require("body-parser"),
-      mongoose   = require("mongoose"),
-      app        = express();
+const methodOverride = require("method-override"),
+      LocalStrategy  = require("passport-local"),
+      bodyParser     = require("body-parser"),
+      mongoose       = require("mongoose"),
+      passport       = require("passport"),
+      express        = require("express"),
+      User           = require("./models/user"),
+      app            = express();
+      
 
 //requiring routes
 const cardRoutes = require("./routes/cards"),
-   commentRoutes = require("./routes/comments");
+   commentRoutes = require("./routes/comments"),
+   indexRoutes      = require("./routes/index");
      
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/hextcg_spoilers");
@@ -15,15 +20,25 @@ app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");      
 
+
+
+//passport config
+app.use(require("express-session")({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use("/", indexRoutes);
 app.use("/cards", cardRoutes);
 app.use("/cards/:id/comments", commentRoutes);
 
 
-
-//root route
-app.get("/", (req, res) => {
-    res.render("landing");
-});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server started");
---
+++ routes/index.js
@@ -0,0 +1,46 @@
+const express  = require("express"),
+      passport = require("passport"),
+      User     = require("../models/user"),
+      router   = express.Router();
+
+
+//root route
+router.get("/", (req, res) => {
+    res.render("landing");
+});   
+
+//register form         
+router.get("/register", (req, res) => {
+    res.render("register");
+});
+
+//register logic
+router.post("/register", (req, res) => {
+    let newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, (err, user) => {
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, () => {
+            res.redirect("/cards");
+        });
+    });
+});
+
+//login page
+router.get("/login", (req, res) => {
+   res.render("login"); 
+});
+
+//login logic
+router.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/cards",
+        failureRedirect: "/login",
+        
+    }), (req, res) => {
+    
+});
+
+module.exports = router;
