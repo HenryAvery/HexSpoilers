2afcc6500c2d9e60e7a1f26f7e67cf0b40d7f6a9
Henry Avery <henryaverydev@gmail.com>
2019-04-10 10:06:53 +0000

    correct mongodb uri

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const cardRoutes = require("./routes/cards"),
    indexRoutes      = require("./routes/index");
      
 mongoose.Promise = global.Promise;
-mongoose.connect("MONGODB_URI");
+mongoose.connect(process.env.MONGODB_URI);
 app.use(bodyParser.urlencoded({extended: true}));   
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
